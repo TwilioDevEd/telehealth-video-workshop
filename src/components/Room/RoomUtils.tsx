67ee1b49f1390560287d0cb1ea17009c0eb45699
twilio-jaharap <jpagadipaala@twilio.com>
2021-09-24 07:08:56 -0700

    code for invite function

--- src/components/Room/RoomUtils.tsx
+++ src/components/Room/RoomUtils.tsx
@@ -4,3 +4,23 @@ export function checkPatient() {
   const sp = new URLSearchParams(window.location.search);
   return sp.get('persona') === 'patient';
 }
+
+export function getPatientName() {
+  const sp = new URLSearchParams(window.location.search);
+  return sp.get('name');
+}
+
+export function getThirdPartyURL(name: string) {
+  const sp = new URLSearchParams(window.location.search);
+  const room = `${sp.get('room')}`;
+  const persona = 'thirdparty';
+  const urlString = window.location.href;
+  let urlPrefix = urlString.substr(0, urlString.indexOf('?'));
+  const passcode = window.sessionStorage.getItem('passcode');
+
+  const thirdPartyUrl = `${urlPrefix}?room=${encodeURIComponent(room)}&persona=${persona}&name=${encodeURIComponent(
+    name
+  )}&passcode=${passcode}`;
+  console.log('URL', thirdPartyUrl);
+  return thirdPartyUrl;
+}
